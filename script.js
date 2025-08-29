e9f869e3b54bb29c66f20986227efbb2d6a0e0e8
tanin3812 <tanin15-3812@diu.edu.bd>
2025-08-29 18:06:28 +0600

    added all functionalities copy button and clear history

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
+// heart count functionality
 let heartCount = 0;
 
 const heart = document.getElementById('heart-count')
-
-
 const allHeart = document.querySelectorAll('.fa-heart');
 for(const hearts of allHeart){
     hearts.addEventListener('click', function(){
@@ -16,45 +15,59 @@ for(const hearts of allHeart){
 }
 
 
+// call and history functionality
 let coinCount = 100;
-const coin = document.getElementById('coin-count')
 
+const coin = document.getElementById('coin-count')
 const callButtons = document.querySelectorAll('.call-btn');
 const historyList = document.getElementById('call-history');
 for(const button of callButtons){
   button.addEventListener('click', function(){
     const card = button.closest('.card');
-    const serviceName = card.querySelector('h3').innerText
-    const serviceNum = card.querySelector('p.text-2xl').innerText
+    const serviceName = card.querySelector('p.category').innerText
+    const serviceNum = card.querySelector('p.service-num').innerText
 
     if(coinCount < 20){
-      alert('Insufficient Coins!');
+      alert('দুঃখিত! আপনার পর্যাপ্ত কয়েন নেই। কল করতে কমপক্ষে ২০টি কয়েন লাগবে।');
       return;
     }
     coinCount = coinCount - 20;
     coin.innerText = coinCount;
-    alert('Calling ' + serviceName + ' (' + serviceNum + ')')
+    alert('Calling ' + serviceName + ' ' + serviceNum + '...')
 
     const time = new Date().toLocaleTimeString();
     const list = document.createElement('li');
-    list.innerText = serviceName + '-' + serviceNum + '     -     (' + time + ')';
-    historyList.appendChild(list);
+    const serviceTitle = card.querySelector('h3').innerText
+    list.className = "flex justify-between items-center  bg-gray-100 p-3 rounded-lg";
+    list.innerHTML = 
+      `<div>
+        <p class="font-medium">${serviceTitle}</p>
+        <p class="text-gray-500">${serviceNum}</p>
+      </div>
+      <span class="text-gray-500">${time}</span>`
+      historyList.prepend(list);
   })
 }
 
 
+// copy functionality
+let copyCount = 0;
+
+const navCopyButton = document.getElementById('nav-copy-btn')
+const copyButtons = document.querySelectorAll('.copy-btn');
+for(const btn of copyButtons){
+    btn.addEventListener('click', function () {
+    const number = btn.parentElement.parentElement.querySelector('p.text-2xl').innerText;
+    copyCount++;
+    navCopyButton.innerText = copyCount + ' Copy';
+    navigator.clipboard.writeText(number).then(() => {
+      alert('নম্বর কপি হয়েছেঃ ' + number)
+    })
+  });
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// clear history
+document.getElementById('clear-history-btn').addEventListener('click', function(){
+  historyList.innerHTML = " ";
+})
